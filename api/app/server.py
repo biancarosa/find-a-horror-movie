8b07a8f360407973eb4a3b2077977796c54c7743
Bianca Rosa <me@biancarosa.com.br>
2017-12-21 01:15:31 -0200

    Add some base code

--- api/app/server.py
+++ api/app/server.py
@@ -4,9 +4,15 @@
 Starts the Flask application
 """
 
+import os
+import json
+import random
+
+import requests
 from flask import Flask, jsonify
 
 api = Flask(__name__)
+TMDB_URL = "https://api.themoviedb.org/3/discover/movie?api_key={}&with_genres=27&page={}"
 
 
 @api.route('/', methods=['GET'])
@@ -16,3 +22,13 @@ def health_check():
         "message": "I'm alive and well, thank you very much for caring!"
     })
     
+@api.route('/discover', methods=['GET'])
+def discover():
+    """Returns a random horror movie"""
+    page = random.randint(1, 883)
+    url = TMDB_URL.format(os.getenv('API_KEY'), page)
+    result = requests.get(url)
+    data = json.loads(result.text)
+    rand = random.randint(0, len(data["results"])-1)
+    movie = data["results"][rand]
+    return jsonify(movie)
